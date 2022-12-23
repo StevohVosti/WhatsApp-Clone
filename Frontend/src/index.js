99198b23f064039be5a8520642461c774d8b6394
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2022-12-23 22:53:57 +0300

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if(process.env === "production");
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
